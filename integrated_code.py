90439554cbafc22c2e3bb63fe10dfde2488d3248
Ayush Jain <ayushjain4484@gmail.com>
2021-08-19 16:25:01 +0200

    Update integrated_code.py

--- integrated_code.py
+++ integrated_code.py
@@ -12,21 +12,11 @@ GPIO.setup(11,GPIO.IN)
 GPIO.setup(13,GPIO.OUT)
 
 #defining Functions
-def emailpir():
-    content = 'Someone is trying to enter your house'
+def email(content):
     mail = smtplib.SMTP('smtp.gmail.com',587)
     mail.ehlo()
     mail.starttls()
-    mail.login('homealertsss@gmail.com','A1b2c3d4#')
-    mail.sendmail('HomeSecurity','ayushjain4484@gmail.com',content)
-    mail.close()
-
-def emailmq2():
-    content = 'Smoke Detected in your house'
-    mail = smtplib.SMTP('smtp.gmail.com',587)
-    mail.ehlo()
-    mail.starttls()
-    mail.login('ayushforibm@gmail.com','A1b2c3d4#')
+    mail.login('user@gmail.com','password')
     mail.sendmail('HomeSecurity','ayushjain4484@gmail.com',content)
     mail.close()
 
@@ -36,7 +26,7 @@ def alert():
     GPIO.output(13,False)
 
 #establishing Database Connection
-conn= MySQLdb.connect("localhost","root","raspberry","testwebsite")
+conn= MySQLdb.connect("localhost","user","password","database")
 c = conn.cursor()
 sqlmq="insert into mq2_tab(ch)values(%s)"
 sqlpir="insert into pir_tab(ch)values(%s)"
@@ -50,14 +40,14 @@ while True:
         conn.commit()
         print "Smoke Detected"
         alert()#LED/BUZZER
-        emailmq2()#sending email
+        email('Smoke Detected in your house')#sending email
         time.sleep(3)
     if p==1:
         c.executemany(sqlpir,"1")
         conn.commit()
         print "intruder Detected"
         alert()#LED/BUZZER
-        emailpir()#sending email
+        email('intruder Alert')#sending email
         time.sleep(3)
 
 
